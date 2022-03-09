540fd581a059c59b551a1de10ec34780935fbd11
Emmanuel Onyeka Eboh <ecolejnr007@gmail.com>
2022-03-09 20:08:15 +0100

    modified some file

--- src/components/peer/LocalPeer.jsx
+++ src/components/peer/LocalPeer.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useVideo } from '@100mslive/react-sdk';
 
 import HandRaiseBadge from './HandRaiseBadge';
 
-// hook for styling
-import useStyles from './styles';
+
 
 const LocalPeer = ({localpeer}) => {
 
